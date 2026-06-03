369a9b7400a341b46225422ca430c5d7ebfccc47
Hosea Hailu <hoseascale1@outlook.com>
2026-06-03 18:12:22 +0100

    addded logical operators and for and while loops

---
+++ ch4_loops/for.py
@@ -0,0 +1,13 @@
+print("Alice")
+print("Bob")
+print("Charlie")
+print("David")
+print("Emma")
+
+for i in range(4):
+    print("Frank")
+    print("Grace")
+
+print("Henry")
+print("Isabella")
+print("Jack")
---
+++ ch4_loops/while.py
@@ -0,0 +1,16 @@
+print("Alice")
+print("Bob")
+print("Charlie")
+print("David")
+print("Emma")
+
+i = 0
+
+while i < 5:
+    print("Frank")
+    print("Grace")
+    i = i + 1
+
+print("Henry")
+print("Isabella")
+print("Jack")
