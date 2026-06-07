856f922ee5c4ee237af0412ca93610e9b440a7be
Hosea Hailu <hoseascale1@outlook.com>
2026-06-07 12:42:46 +0100

    changed new array files

---
+++ AvacComputers/AvacStartup.py
@@ -0,0 +1,11 @@
+print("Avac Comuputers")
+
+PasswordSaved = True
+
+if PasswordSaved == False:
+    NewPassword = input("enter new password: ")
+    Password = NewPassword
+
+else:
+    Password = input("enter password: ")
+    Password = "67"
--- ch6_arrays(or_lists)/script.py
+++ ch6_arrays(or_lists)/script.py
@@ -2,9 +2,13 @@
 
 # print(numbers[4])
 
-friends = ["hosea","anwar","timmy","sebby","jeff"]
-friends[1] = "Ahamed"
-friends.append("doe")
-friends.remove("sebby")
-print(friends)
-print(friends[1])
+# friends = ["hosea", "anwar", "timmy", "sebby", "jeff"]
+# friends[1] = "Ahamed"
+# friends.append("doe")
+# friends.remove("sebby")
+# print(friends)
+# print(friends[1])
+
+ages = [11, 12, 26, 18, 67, 2]
+ages[0] = ages[0] +1
+print(ages)
