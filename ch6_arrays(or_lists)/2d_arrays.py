3139d90cb8da071b013d216247874a42720eede8
Hosea Hailu <hoseascale1@outlook.com>
2026-06-14 12:58:56 +0100

    Started new chapter on file handeling andadded new script in arrays

---
+++ ch6_arrays(or_lists)/2d_arrays.py
@@ -0,0 +1,17 @@
+# maitrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print(maitrix[1][2]) #5
+
+names = [
+    ["John", "Sarah", "Mike"],
+    ["Emma", "Lily", "James"],
+    ["Alex", "Sophia", "Daniel"],
+]
+
+print(names[0][0])  # John
+print(names[1][2])  # James
+print(names[2][1])  # Sophia
---
+++ ch7_file_handeling/script.py
@@ -0,0 +1,11 @@
+# students = open("list_of_students.txt", "r")
+# print(students.read())
+
+# Hello = open("Hello_world.py", "x")
+# Hello.close()
+
+# students = open("list_of_students.txt", "r")
+
+# print(students.read())
+
+# students.close()
