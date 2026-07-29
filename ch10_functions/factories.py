e606685cf98cc0612236a4a03895ff7fce91fbb7
Hosea Hailu <hoseascale1@outlook.com>
2026-07-29 18:27:22 +0100

    added new scripts to ch10

---
+++ ch10_functions/factories.py
@@ -0,0 +1,26 @@
+# def multiply(num1, num2):
+#     result = num1 * num2
+#     print(result)
+
+# def addtion(num1, num2):
+#     result = num1 + num2
+#     print(result)
+
+# def subtraction(num1, num2):
+#     result = num1 - num2
+#     print(result)
+
+# def divide(num1, num2):
+#     result = num1 / num2
+#     print(result)
+
+# multiply(3, 6)
+# multiply(4, 12)
+# multiply(3, 8)
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+output = multiply(3,6)
+print(output)
---
+++ ch10_functions/functions_continents.py
@@ -0,0 +1,8 @@
+def Austrailia():
+    print("Also Known as Ocenia")
+    print("Main country: Sydeny")
+
+
+print("Austrailia:")
+Austrailia()
+
