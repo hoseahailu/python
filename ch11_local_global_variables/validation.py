ef5f305f4cbca413bdc475181ab6475350dfb3e2
Hosea Hailu <hoseascale1@outlook.com>
2026-08-05 18:55:57 +0100

    added new scripts in ch11

---
+++ ch11_local_global_variables/validation.py
@@ -0,0 +1,39 @@
+# name = input("Enter your name: ")
+
+# if name == "":
+#     print("Error: You must enter your name.")
+# else:
+#     print("Hello", name)
+
+#Range Check
+# age = int(input("Enter your age: "))
+
+# if 0 <= age <= 120:
+#     print("Valid age.")
+# else:
+#     print("Age is outside the valid range.")
+
+
+#Length check
+# password = input("Create a password: ")
+
+# if len(password) >= 8:
+#     print("Password accepted.")
+# else:
+#     print("Password must be at least 8 characters.")
+
+#Format check
+# email = input("Enter your email: ")
+
+# if "@" in email and "." in email:
+#     print("Email looks valid.")
+# else:
+#     print("Invalid email.")
+
+#Type (Digit) Check
+# number = input("Enter your favourite number: ")
+
+# if number.isdigit():
+#     print("You entered", int(number))
+# else:
+#     print("Numbers only!")
